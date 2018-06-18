59c2952786636dbade2c8acf261d6e36fe219dfb
fatfisz <fatfisz@gmail.com>
2018-06-18 23:19:02 +0200

    Do styled-components detection and pray that it works

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,35 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { Fragment } from 'react';
-import { ServerStyleSheet } from 'styled-components';
 import flush from 'styled-jsx/server';
+
+function getStyledComponents() {
+  try {
+    const { ServerStyleSheet } = __non_webpack_require__('styled-components');
+    return { ServerStyleSheet };
+  } catch (error) {
+    return null;
+  }
+}
 
 export default class extends Document {
   static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-    const jsxStyles = flush();
-    const styledStyles = sheet.getStyleElement();
-    const styles = (
-      <Fragment>
-        {jsxStyles}
-        {styledStyles}
-      </Fragment>
-    );
+    const styledComponents = getStyledComponents();
+    if (styledComponents !== null) {
+      const sheet = new styledComponents.ServerStyleSheet();
+      const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+      const jsxStyles = flush();
+      const styledStyles = sheet.getStyleElement();
+      const styles = (
+        <Fragment>
+          {jsxStyles}
+          {styledStyles}
+        </Fragment>
+      );
+      return { ...page, styles };
+    }
+
+    const page = renderPage();
+    const styles = flush();
     return { ...page, styles };
   }
 }
